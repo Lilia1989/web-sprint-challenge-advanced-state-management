af8fe9688361dcd0d5c780b40962641e718600e2
Lilia Ovcharenko <lilia.ovcharenko@gmail.com>
2021-05-01 20:52:28 -0700

    all set with the redux store, successfully connected reducers with actions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
+
 class App extends Component {
   render() {
     return (
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,32 @@
-
+import { FETCHING_FAILURE, FETCHING_START, FETCHING_SUCCESS } from '../actions'
 export const initialState = {
+    smurfs: [],
+    loading: false,
+    error: ""
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action)=>{
+    switch (action.type) {
+        case FETCHING_START:
+            return {...state, loading: true}
+        case FETCHING_SUCCESS:
+            return {...state, loading: false, smurfs: action.payload }  
+        case FETCHING_FAILURE:
+            return {...state, loading: false, error: action.payload}  
+        default:
+            return state;        
+    }
 }
 
 export default reducer;
 
+
+
+
 //Task List:
 //1. Adds the following state values into the initialState:
 //  - an array of smurfs
+
 //  - a boolean indicating if the app is loading
 //  - a string indicating a possible error message
 
@@ -19,3 +36,4 @@ export default reducer;
 //5. Add in a reducer cases to accomidate the failed smurf api fetch.
 //6. Add in a reducer case to accomidate adding a smurf (including the name, nickname, position, summary and an internally generated id) into your smurf list.
 //7. Add in a reducer case that adds in a value to the error message.
+
